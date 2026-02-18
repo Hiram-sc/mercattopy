cddf56088f78003b104f84e526bfe1928d3a38b4
Hiram <hscobuci@gmail.com>
2026-02-18 19:29:20 -0300

    corrige dados do banco e roda informações

--- catalog/models.py
+++ catalog/models.py
@@ -10,8 +10,8 @@ class Categoria(models.Model):
     
 class Produto(models.Model):
     nome = models.CharField(max_length=100, unique=True)
-    descricao = models.TextField(blank=True, null=True)
-    preco = models.DecimalField(max_digits=5 ,decimal_places=2)
+    descricao = models.TextField(blank=True, null=True) 
+    preco = models.DecimalField(max_digits=6 ,decimal_places=2)
     quantidade_estoque = models.IntegerField(default=0)
     status = models.BooleanField(default=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='produtos')
