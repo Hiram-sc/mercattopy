a6b7d8816eb6c1c3d4753a2e48ab51e8ba714de8
Hiram <hscobuci@gmail.com>
2026-02-18 15:32:06 -0300

    initial commit

---
+++ catalog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100, unique=True, null=False)
+    descricao = models.TextField(blank=True, null=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.nome}'
+    
+class Produto(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    descricao = models.TextField(blank=True, null=True)
+    preco = models.DecimalField(max_digits=5 ,decimal_places=2)
+    quantidade_estoque = models.IntegerField(default=0)
+    status = models.BooleanField(default=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='produtos')
+    
+    def __str__(self):
+        return f'{self.nome}'
