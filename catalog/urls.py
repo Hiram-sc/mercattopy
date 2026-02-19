07e1eb13e9fefbfe696a7bcdabd8d643c092f693
Hiram <hscobuci@gmail.com>
2026-02-18 21:36:59 -0300

    implementa rotas e views básicas do catálogo

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('categorias/', views.lista_categoria),
+    path('produtos/', views.lista_produto),
+    path('produtos/<int:id>/', views.item_produto)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 
+from django.shortcuts import render
+from catalog.models import Categoria, Produto
+
+def lista_categoria(request):
+    categorias = Categoria.objects.all()
+    return HttpResponse(categorias)
+
+def lista_produto(request):
+    produtos = Produto.objects.all()
+    return HttpResponse(produtos)
+
+def item_produto(request, id):
+    item = Produto.objects.get(id=id)
+    return HttpResponse(item)
